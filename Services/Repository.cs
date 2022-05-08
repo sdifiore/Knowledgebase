1895fc4cd112aae9f463824eca106e5eb335d6ff
Sergio Di Fiore <sergiodifiore@gmail.com>
2022-05-08 20:33:46 -0300

    Repository Service

---
+++ Services/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Knowledge.Models;
+using Knowledgebase.Data;
+
+namespace Knowledgebase.Services
+{
+    public class Repository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Framework> GetFrameworks()
+        {
+            return _context.Frameworks;
+        }
+    }
+}
