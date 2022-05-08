346206ef070e318e4c53677f0c94acfd5f221a33
Sergio Di Fiore <sergiodifiore@gmail.com>
2022-05-08 20:07:07 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Knowledgebase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
