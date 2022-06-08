05ce26e6a34a81e9fa40591ba43adb8827f8a623
Sergio Di Fiore <sergiodifiore@gmail.com>
2022-06-08 20:26:47 -0300

    CRUD Plataformas

--- Controllers/PlataformasController.cs
+++ Controllers/PlataformasController.cs
@@ -1,91 +1,95 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using KnowledgeBase.Models;
 using Knowledgebase.Data;
 
 namespace Knowledgebase.Controllers
 {
-    public class ErrosController : Controller
+    public class PlataformasController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public ErrosController(ApplicationDbContext context)
+        public PlataformasController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: Erros
+        // GET: Plataformas
         public async Task<IActionResult> Index()
         {
-              return _context.Erros != null ? 
-                          View(await _context.Erros.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Erros'  is null.");
+            var applicationDbContext = _context.Plataformas.Include(p => p.Framework);
+            return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Erros/Details/5
+        // GET: Plataformas/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Erros == null)
+            if (id == null || _context.Plataformas == null)
             {
                 return NotFound();
             }
 
-            var erro = await _context.Erros
+            var plataforma = await _context.Plataformas
+                .Include(p => p.Framework)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (erro == null)
+            if (plataforma == null)
             {
                 return NotFound();
             }
 
-            return View(erro);
+            return View(plataforma);
         }
 
-        // GET: Erros/Create
+        // GET: Plataformas/Create
         public IActionResult Create()
         {
+            ViewData["FrameworkId"] = new SelectList(_context.Frameworks, "Id", "Apelido");
             return View();
         }
 
-        // POST: Erros/Create
+        // POST: Plataformas/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Codigo,Fonte")] Erro erro)
+        public async Task<IActionResult> Create([Bind("Id,Apelido,Descricao,Versao,FrameworkId")] Plataforma plataforma)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(erro);
+                _context.Add(plataforma);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(erro);
+            ViewData["FrameworkId"] = new SelectList(_context.Frameworks, "Id", "Apelido", plataforma.FrameworkId);
+            return View(plataforma);
         }
 
-        // GET: Erros/Edit/5
+        // GET: Plataformas/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Erros == null)
+            if (id == null || _context.Plataformas == null)
             {
                 return NotFound();
             }
 
-            var erro = await _context.Erros.FindAsync(id);
-            if (erro == null)
+            var plataforma = await _context.Plataformas.FindAsync(id);
+            if (plataforma == null)
             {
                 return NotFound();
             }
-            return View(erro);
+            ViewData["FrameworkId"] = new SelectList(_context.Frameworks, "Id", "Apelido", plataforma.FrameworkId);
+            return View(plataforma);
         }
 
-        // POST: Erros/Edit/5
+        // POST: Plataformas/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Codigo,Fonte")] Erro erro)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Apelido,Descricao,Versao,FrameworkId")] Plataforma plataforma)
         {
-            if (id != erro.Id)
+            if (id != plataforma.Id)
             {
                 return NotFound();
             }
@@ -94,12 +98,12 @@ namespace Knowledgebase.Controllers
             {
                 try
                 {
-                    _context.Update(erro);
+                    _context.Update(plataforma);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ErroExists(erro.Id))
+                    if (!PlataformaExists(plataforma.Id))
                     {
                         return NotFound();
                     }
@@ -110,49 +114,51 @@ namespace Knowledgebase.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(erro);
+            ViewData["FrameworkId"] = new SelectList(_context.Frameworks, "Id", "Apelido", plataforma.FrameworkId);
+            return View(plataforma);
         }
 
-        // GET: Erros/Delete/5
+        // GET: Plataformas/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Erros == null)
+            if (id == null || _context.Plataformas == null)
             {
                 return NotFound();
             }
 
-            var erro = await _context.Erros
+            var plataforma = await _context.Plataformas
+                .Include(p => p.Framework)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (erro == null)
+            if (plataforma == null)
             {
                 return NotFound();
             }
 
-            return View(erro);
+            return View(plataforma);
         }
 
-        // POST: Erros/Delete/5
+        // POST: Plataformas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Erros == null)
+            if (_context.Plataformas == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.Erros'  is null.");
+                return Problem("Entity set 'ApplicationDbContext.Plataformas'  is null.");
             }
-            var erro = await _context.Erros.FindAsync(id);
-            if (erro != null)
+            var plataforma = await _context.Plataformas.FindAsync(id);
+            if (plataforma != null)
             {
-                _context.Erros.Remove(erro);
+                _context.Plataformas.Remove(plataforma);
             }
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool ErroExists(int id)
+        private bool PlataformaExists(int id)
         {
-          return (_context.Erros?.Any(e => e.Id == id)).GetValueOrDefault();
+          return (_context.Plataformas?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
