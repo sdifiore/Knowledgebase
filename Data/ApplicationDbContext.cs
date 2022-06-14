725cadf75682c028492dc3b021e66ca9006c6fdb
Sergio Di Fiore <sergiodifiore@gmail.com>
2022-06-13 22:12:52 -0300

    Controladoras e Views resetadas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,11 +11,11 @@ namespace Knowledgebase.Data
         {
         }
 
-        public DbSet<Artigo> Artigos { get; set; }
-        public DbSet<Autor> Autores { get; set; }
-        public DbSet<Erro> Erros { get; set; }
-        public DbSet<Fonte> Fontes { get; set; }
-        public DbSet<Framework> Frameworks { get; set; }
-        public DbSet<Plataforma> Plataformas { get; set; }
+        public DbSet<Artigo>? Artigos { get; set; }
+        public DbSet<Autor>? Autores { get; set; }
+        public DbSet<Erro>? Erros { get; set; }
+        public DbSet<Fonte>? Fontes { get; set; }
+        public DbSet<Framework>? Frameworks { get; set; }
+        public DbSet<Plataforma>? Plataformas { get; set; }
     }
 }
