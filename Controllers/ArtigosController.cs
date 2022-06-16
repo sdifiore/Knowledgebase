6b302cad2c5e08a9c5001d13433c744b993f8c79
Sergio Di Fiore <sergiodifiore@gmail.com>
2022-06-15 21:51:23 -0300

    CRUD Artigos e Autores II

---
+++ Controllers/ArtigosController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KnowledgeBase.Models;
+using Knowledgebase.Data;
+
+namespace Knowledgebase.Controllers
+{
+    public class ArtigosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtigosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artigos
+        public async Task<IActionResult> Index()
+        {
+              return _context.Artigos != null ? 
+                          View(await _context.Artigos.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Artigos'  is null.");
+        }
+
+        // GET: Artigos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Artigos == null)
+            {
+                return NotFound();
+            }
+
+            var artigo = await _context.Artigos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (artigo == null)
+            {
+                return NotFound();
+            }
+
+            return View(artigo);
+        }
+
+        // GET: Artigos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artigos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Chamada,Corpo,DiaMesAno")] Artigo artigo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(artigo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artigo);
+        }
+
+        // GET: Artigos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Artigos == null)
+            {
+                return NotFound();
+            }
+
+            var artigo = await _context.Artigos.FindAsync(id);
+            if (artigo == null)
+            {
+                return NotFound();
+            }
+            return View(artigo);
+        }
+
+        // POST: Artigos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Chamada,Corpo,DiaMesAno")] Artigo artigo)
+        {
+            if (id != artigo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artigo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtigoExists(artigo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artigo);
+        }
+
+        // GET: Artigos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Artigos == null)
+            {
+                return NotFound();
+            }
+
+            var artigo = await _context.Artigos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (artigo == null)
+            {
+                return NotFound();
+            }
+
+            return View(artigo);
+        }
+
+        // POST: Artigos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Artigos == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Artigos'  is null.");
+            }
+            var artigo = await _context.Artigos.FindAsync(id);
+            if (artigo != null)
+            {
+                _context.Artigos.Remove(artigo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtigoExists(int id)
+        {
+          return (_context.Artigos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
